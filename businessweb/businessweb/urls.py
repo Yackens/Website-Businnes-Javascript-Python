01e333726c6617d88a3fd8d0a7f85f1dd15a333f
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-05-17 00:47:42 +0200

    Changes business website

--- businessweb/businessweb/urls.py
+++ businessweb/businessweb/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import *
 
+
 urlpatterns = [
     path("", include("core.urls")),
     path('admin/', admin.site.urls),
