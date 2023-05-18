25da5492fef1d7f3276443c53b5dca23923c7450
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-05-19 01:42:53 +0200

    Changes services business website

---
+++ businessweb/services/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    subtitle = models.CharField(max_length=200, verbose_name="Subtitle")
+    content = models.TextField(verbose_name="Content")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Modified Date")
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
