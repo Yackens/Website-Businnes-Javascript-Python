ef311319e19cabf875f9298508332cc643782370
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-05-28 20:36:38 +0200

    Changes business website

--- businessweb/pages/models.py
+++ businessweb/pages/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Page(models.Model):
-    key = models.SlugField(max_length=100, verbose_name="Key", default="Hello21__")
     title = models.CharField(max_length=100, verbose_name="Title")
     content = models.TextField(verbose_name="Content")
-    link = models.URLField(max_length=200, verbose_name="URL", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
     updated = models.DateTimeField(auto_now=True, verbose_name="Modified Date")
 
@@ -16,4 +14,3 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
-    
--- businessweb/pages/urls.py
+++ businessweb/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from pages import views
+from . import views
 
 urlpatterns = [
-    path("<int:page_id>/", views.page, name = "page")
+    path('<int:page_id>/', views.page, name="page"),
 ]
